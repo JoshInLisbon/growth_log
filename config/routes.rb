f89933b3d12662581066ea0e5995870741b403ca
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2022-03-05 11:55:19 +0000

    Projects new and index request specs, failing create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#homepage'
 
-  resources :projects, only: [:create, :indes]
+  resources :projects, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :request do
-  describe "GET /create" do
+  describe "GET /projects/new" do
     it "returns http success" do
+      get "/projects/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /create" do
+    pending "returns http success" do
       get "/projects/create"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /index" do
+  describe "GET /projects" do
     it "returns http success" do
-      get "/projects/index"
+      get "/projects"
       expect(response).to have_http_status(:success)
     end
   end
